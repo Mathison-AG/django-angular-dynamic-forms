a3789276265ee95c2a0b4491dceb4c3d44bab505
Mirek Simek <miroslav.simek@vscht.cz>
2018-02-03 17:06:15 +0100

    removed callbacks in favour of observables
    Do not forget to subscribe the dialog!

--- lib/django-form/django-form-dialog.service.ts
+++ lib/django-form/django-form-dialog.service.ts
@@ -4,32 +4,50 @@ import 'rxjs/add/observable/throw';
 import {MatDialog} from '@angular/material';
 import {DialogDjangoFormComponent} from './impl/dialog-django-form.component';
 import {Injectable} from '@angular/core';
+import {isUndefined} from 'util';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class DjangoFormDialogService {
-
     constructor(private dialog: MatDialog) {
     }
 
-    public open(django_url: string, submit?: (data, response) => void,
-                cancel?: (data) => void,
-                extra_options?: {config?: any, form_title?: string}) {
+    public open(django_url: string,
+                extra_options?: {
+                    submit?: (data, response) => void,
+                    cancel?: (data) => void,
+                    config?: any;
+                    form_title?: string;
+                    extra_form_data?: any;
+                    fields?: string[],
+                    initial_data_transformation? : (any) => any
+                }) {
+
+        if (!extra_options) {
+            extra_options = {};
+        }
 
         const dialogRef = this.dialog.open(DialogDjangoFormComponent, {
             // width: '250px',
             data: {
                 django_url: django_url,
-                config: extra_options ? extra_options.config : null,
-                form_title: extra_options ? extra_options.form_title : null,
+                config: extra_options.config,
+                form_title: extra_options.form_title,
+                extra_form_data: extra_options.extra_form_data || {},
+                fields: extra_options.fields || [],
+                initial_data_transformation: extra_options.initial_data_transformation
             }
         });
 
-        dialogRef.afterClosed().subscribe(result => {
-            if (result.cancel && cancel) {
-                cancel(result.data);
-            } else if (submit) {
-                submit(result.data, result.response);
-            }
-        });
+        return dialogRef
+            .afterClosed()
+            .map(result => result || {cancel: true, data: undefined})
+            .do(result => {
+                if (result.cancel && extra_options.cancel) {
+                    extra_options.cancel(result.data);
+                } else if (extra_options.submit) {
+                    extra_options.submit(result.data, result.response);
+                }
+            });
     }
 }
