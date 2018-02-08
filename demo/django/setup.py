32faa43b96cd128fe062afb176302f96ffe321bc
Mirek Simek <miroslav.simek@vscht.cz>
2018-02-08 17:34:38 +0100

    setup fixes

--- demo/django/setup.py
+++ demo/django/setup.py
@@ -67,7 +67,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['angular_dynamic_forms'],
+    packages=['api', 'testserver'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
