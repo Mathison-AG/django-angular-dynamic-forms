f94d0fc6c660a161c0af081a7404e2ed167466d6
Mirek Simek <miroslav.simek@vscht.cz>
2018-02-11 16:07:50 +0100

    Adding unicorn

--- demo/docker/web/run.sh
+++ demo/docker/web/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 cd /data/django-angular-dynamic-forms/demo/django
-python3 manage.py runserver 0.0.0.0:8000
+# python3 manage.py runserver 0.0.0.0:8000
+
+gunicorn -b 0.0.0.0:8000 -w 2 testserver.wsgi
