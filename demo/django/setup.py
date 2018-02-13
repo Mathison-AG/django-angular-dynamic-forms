75ac46907f4ccaf654b961ebb455e3eb34d56519
Mirek Simek <miroslav.simek@vscht.cz>
2018-02-13 21:07:15 +0100

    version 0.5.32: changed to use a different library builder, refactored to use camelCase everywhere

--- demo/django/setup.py
+++ demo/django/setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.31',
+    version='0.5.32',
 
     description='Angular forms for django rest framework',
     long_description=long_description,
