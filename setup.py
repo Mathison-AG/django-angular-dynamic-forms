2574b552ee0150df16b010468119ba694faa2ce5
Mirek Simek <miroslav.simek@vscht.cz>
2018-05-31 14:16:34 +0200

    version 1.1.2:

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.1',
+    version='1.1.2',
 
     description='Django Rest Framework meets Angular 5 material.io dynamic forms - rapid development of create and edit dialogs',
     long_description=long_description,
