65f61ded96024a6d6f0707f3419d93249b0dfe62
Mirek Simek <miroslav.simek@vscht.cz>
2018-06-13 09:56:30 +0200

    version 6.0.3:

--- demo/django/setup.py
+++ demo/django/setup.py
@@ -25,7 +25,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='6.0.1',
+    version='6.0.3',
 
     description='Angular forms for django rest framework',
     long_description=long_description,
